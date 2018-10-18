089025b5c83b87add4e25505e87fcbe09efe8d7d
Filippo Volpe <filippovolpe98@gmail.com>
2018-10-18 09:48:53 +0200

    try2

--- Exp1/functions.py
+++ Exp1/functions.py
@@ -48,14 +48,17 @@ def cov(a,b,ddof=0):
     cov_ab=np.sum((a-ma) * (b-mb))/(len(a)-ddof)
     return cov_ab
 
-    
+
 class DataXY:
-    def __init__(self,x,y,dx=0,dy=0):
+    def __init__(self,x,y,dx=0,dy=0,name="DataXY",x_label="x",y_label="y"):
         self.x=x
         self.y=y
         self.dx=dx*np.ones(len(x))
         self.dy=dy*np.ones(len(x))
-        
+        self.x_label=x_label
+        self.y_label=y_label
+        self.name=name
+
     def getLinearRegressionAB(self,w=None):
         if w is None:
             w=1/self.dy**2
@@ -76,38 +79,47 @@ class DataXY:
         dA= np.sqrt(np.sum(((self.x)**2)*w) / dw)
         dB= np.sqrt(np.sum(w)/dw)
         return A,B,dA,dB
-    
-    def getModelAB(self):
-        A,B,dA,dB = self.getLinearRegressionAB()
-        return A*self.x+B
-    
+
+
     def getChi2(self):
         return np.sum(((self.getModel() - self.y)**2)/(self.dy**2))
-    
-    def getFitPlot(self):
+
+    def getChi2Red(self,ddof=0):
+        return self.getChi2()/(len(self.x)-ddof)
+
+    def getModel(self,x=None):
         A,B,dA,dB = self.getLinearRegressionAB()
-        
-        fig = plt.figure()
-        
-        ax_top = fig.add_subplot(2,1,1)
-        
-        DX = np.max(self.x)-np.min(self.x)
-        x_mod = np.min(self.x) - .05*DX + 1.1 * DX * np.array([0,1]) # TODO: what?
-        y_mod = B + A*x_mod
-        ax_top.plot(x_mod,y_mod)
-        ax_top.set_ylabel("dati")
+        if x is None:
+            return A+B*self.x
+        else:
+            return A+B*x
+
+    def getFitPlot(self,fmt="b.",fmt_m="r,-",x_lim=None):
+        A,B,dA,dB = self.getLinearRegressionAB()
+
+        fig,(ax_top,ax_bot) = plt.subplots(2,1, gridspec_kw={'height_ratios':[3,1]})
+        fig.suptitle(self.name)
+
+        ax_top.errorbar(self.x,self.y,xerr=self.dx,yerr=self.dy,fmt=fmt)
+        if x_lim is None:
+            x_lim=ax_top.get_xlim()
+        ax_top.plot(x_lim,\
+                     A+B*np.array(x_lim),\
+                     fmt_m)
+        ax_top.set_xlim(x_lim)
+        ax_top.set_ylabel(self.y_label)
         ax_top.grid()
-        ax_top.set_xlim([np.min(self.x)-.06*DX,np.max(self.x)*.06*DX])
-        
-        res = self.y - (self.getModelAB())
-        
-        dy = np.sqrt(self.dy**2 + (B*self.dx)**2)
-        
-        ax_bot = fig.add_subplot(2,1,2)
-        ax_bot.errorbar(self.x,res,self.dy)
-        ax_bot.set_ylabel("residui")
-        ax_bot.set_xlabel("x")
+        ax_top.ticklabel_format(style="sci",scilimits=(0,0))
+
+        res = self.y - self.getModel()
+        dy_propagated = np.sqrt(self.dy**2 + (B*self.dx)**2)
+
+        ax_bot.errorbar(self.x,res,yerr=dy_propagated,xerr=self.dx,fmt=fmt)
+        ax_bot.axhline(0,color=fmt_m[0])
+        ax_bot.set_xlim(x_lim)
+        ax_bot.set_ylabel("Res "+self.y_label)
+        ax_bot.set_xlabel(self.x_label)
         ax_bot.grid()
-        ax_bot.set_xlim([np.min(self.x)-.06*DX,np.max(self.x)+.06*DX])
-        
-        return fig,ax_top,ax_bot
+
+
+        return fig,(ax_top,ax_bot)
--- Exp1/main_exp2.py
+++ Exp1/main_exp2.py
@@ -6,16 +6,45 @@ Created on Thu Oct 11 09:57:32 2018
 @author: volpe
 """
 
-from functions import DataXY
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from uncertainties import ufloat
 
-def main():
-    x=np.arange(100)
-    y=5+3*x+np.random.rand(len(x))
-    data=DataXY(x,y,1,1)
-    fig,ax1,ax2=data.getFitPlot()
-    plt.show()
+from functions import DataXY
 
-if __name__ == "__main__":
-    main()
+data=[pd.read_csv("data/AmpMon10V5mA.csv"),
+         pd.read_csv("data/AmpMon2V0.5mA.csv"),
+         pd.read_csv("data/AmpVal10V5mA.csv"),
+         pd.read_csv("data/AmpVal2V0.5mA.csv")]
+
+for df in data:
+    df["mA"]*=1e-3
+    df.columns=["V","A"]
+data_fs=np.array([[10,5e-3],[2,500e-6],[10,5e-3],[2,500e-6]])
+data_Delta=np.array([[0.2,100e-6],[0.04,10e-6],[0.2,100e-6],[0.04,10e-6]])
+data_sigma=data_Delta/np.sqrt(12)
+
+dataseries=[DataXY(data[0]["V"],data[0]["A"],\
+                   data_sigma[0][0],data_sigma[0][1],\
+                   x_label="V",y_label="A",\
+                   name="Amperometro a Monte (FS: 10V/5mA)"),\
+            DataXY(data[1]["V"],data[1]["A"],\
+                   data_sigma[1][0],data_sigma[1][1],\
+                   x_label="V",y_label="A",\
+                   name="Amperometro a Monte (FS: 2V/500uA)"),\
+            DataXY(data[2]["V"],data[2]["A"],\
+                   data_sigma[2][0],data_sigma[2][1],\
+                   x_label="V",y_label="A",\
+                   name="Amperometro a Valle (FS: 10V/5mA)"),\
+            DataXY(data[3]["V"],data[3]["A"],\
+                   data_sigma[3][0],data_sigma[3][1],\
+                   x_label="V",y_label="A",\
+                   name="Amperometro a Valle (FS: 2V/500uA)"),\
+            ]
+
+dataseries[0].getFitPlot()[0].savefig("data/AmpMon10V5mA.svg")
+dataseries[1].getFitPlot()[0].savefig("data/AmpMon2V0.5mA.svg")
+dataseries[2].getFitPlot()[0].savefig("data/AmpVal10V5mA.svg")
+dataseries[3].getFitPlot()[0].savefig("data/AmpVal2V0.5mA.svg")
+
