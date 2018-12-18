ee7b72f03727df0baf0ac8dbe91ce350d9e71526
Filippo Volpe <filippovolpe98@gmail.com>
2018-12-18 23:34:59 +0100

    started 6

--- Exp56/main_exp6.py
+++ Exp56/main_exp6.py
@@ -14,5 +14,30 @@ import matplotlib.pyplot as plt
 #%% FBR
 df1=pd.read_csv('data6/FBR.csv').values
 
+Rl=df1[:,0]
+
+fig1=plt.figure()
+fig1.suptitle('In/Out characterisic')
+ax1=fig1.add_subplot(111)
+
+
+ax1.plot(Rl,df1[:,1]*np.sqrt(2),'b.',label='Vin max')
+ax1.plot(Rl,df1[:,2],'r.',label='Vout RMS')
+ax1.set_xscale('log')
+ax1.set_xlabel('R load [Ω]')
+ax1.set_ylabel('V [V]')
+ax1.grid()
+ax1.legend()
+
+
+fig2=plt.figure()
+fig2.suptitle('Ripple out')
+ax2=fig2.add_subplot(111)
+
+ax2.plot(Rl,df1[:,3],'b.')
+ax2.set_xscale('log')
+ax2.set_xlabel('R load [Ω]')
+ax2.set_ylabel('Vpp [V]')
+ax2.grid()
 
 #%% FBRD
